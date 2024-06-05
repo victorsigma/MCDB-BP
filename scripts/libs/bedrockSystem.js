3b253895ae94de8e0531aa527ad00d46e40078d9
victorsigma <victorossielgaray1@gmail.com>
2024-06-04 20:13:33 -0600

    Actualizacion a BedrockSystem: Mejoras en la logica del codigo y reduccion de parametros.

--- scripts/libs/bedrockSystem.js
+++ scripts/libs/bedrockSystem.js
@@ -6,84 +6,55 @@ class BedrockSystem {
     constructor() {} // Constructor vacío
 
     // Método para registrar mensajes de log
-    log(message, ...optionalParams) {
-        setType('r'); // Establece el tipo de mensaje a "r" (normal)
-        // Verifica si el mensaje es un objeto JSON
-        if (isJSON(message)) {
-            // Formatea los parámetros opcionales si son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                optionalParamsFormat.push(format(element));
-            });
-            // Imprime el mensaje formateado con los parámetros opcionales
-            console.warn(format(message), ...optionalParamsFormat);
-        } else {
-            // Formatea los parámetros opcionales si no son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                if (isJSON(element)) {
-                    optionalParamsFormat.push(format(element));
-                } else {
-                    optionalParamsFormat.push(element);
-                }
-            });
-            // Imprime el mensaje con los parámetros opcionales formateados
-            console.warn(message, ...optionalParamsFormat);
-        }
+    log(...data) {
+        // Establece el tipo de mensaje a "r" (normal)
+        setType('r')
+        const dataFormat = []
+        // Formatea los parámetros si son JSON
+        data.forEach(element => {
+            if (isJSON(element)) {
+                dataFormat.push(format(element))
+            } else {
+                dataFormat.push(element)
+            }
+        });
+        
+        // Imprime el mensaje
+        console.warn(...dataFormat)
     }
 
     // Método para registrar mensajes de advertencia
-    warn(message, ...optionalParams) {
-        setType('6'); // Establece el tipo de mensaje a "6" (advertencia)
-        // Verifica si el mensaje es un objeto JSON
-        if (isJSON(message)) {
-            // Formatea los parámetros opcionales si son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                optionalParamsFormat.push(format(element));
-            });
-            // Imprime el mensaje de advertencia formateado con los parámetros opcionales
-            console.warn(format(message), ...optionalParamsFormat);
-        } else {
-            // Formatea los parámetros opcionales si no son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                if (isJSON(element)) {
-                    optionalParamsFormat.push(format(element));
-                } else {
-                    optionalParamsFormat.push(`§6${element}`);
-                }
-            });
-            // Imprime el mensaje de advertencia con los parámetros opcionales formateados
-            console.warn(`§6${message}`, ...optionalParamsFormat);
-        }
+    warn(...data) {
+        setType('r§6'); // Establece el tipo de mensaje a "r§6" (advertencia)
+        const dataFormat = []
+        // Formatea los parámetros si no son JSON
+        data.forEach(element => {
+            if (isJSON(element)) {
+                dataFormat.push(format(element))
+            } else {
+                dataFormat.push(`§6${element}`)
+            }
+        });
+
+        // Imprime el mensaje de advertencia
+        console.warn(...dataFormat)
     }
 
+
     // Método para registrar mensajes de error
-    error(message, ...optionalParams) {
-        setType('4'); // Establece el tipo de mensaje a "4" (error)
-        // Verifica si el mensaje es un objeto JSON
-        if (isJSON(message)) {
-            // Formatea los parámetros opcionales si son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                optionalParamsFormat.push(format(element));
-            });
-            // Imprime el mensaje de error formateado con los parámetros opcionales
-            console.warn(format(message), ...optionalParamsFormat);
-        } else {
-            // Formatea los parámetros opcionales si no son JSON
-            const optionalParamsFormat = [];
-            optionalParams.forEach(element => {
-                if (isJSON(element)) {
-                    optionalParamsFormat.push(format(element));
-                } else {
-                    optionalParamsFormat.push(`§4${element}`);
-                }
-            });
-            // Imprime el mensaje de error con los parámetros opcionales formateados
-            console.warn(`§4${message}`, ...optionalParamsFormat);
-        }
+    error(...data) {
+        setType('r§4') // Establece el tipo de mensaje a "r§4" (error)
+        const dataFormat = []
+        // Formatea los parámetros si no son JSON
+        data.forEach(element => {
+            if (isJSON(element)) {
+                dataFormat.push(format(element))
+            } else {
+                dataFormat.push(`§4${element}`)
+            }
+        });
+        // Imprime el mensaje de error
+        console.warn(...dataFormat)
     }
 }
 
